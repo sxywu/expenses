c3e3d60c662b28e1cd0b783d0e898f647de121d0
Shirley Wu <shirleyxueyangwu@gmail.com>
2015-02-15 23:13:43 -0800

    add category name

--- scripts/components/AddCategory.jsx
+++ scripts/components/AddCategory.jsx
@@ -2,15 +2,33 @@ var React = require('react');
 var ViewActionCreators = require('../actions/ViewActionCreators');
 
 var AddCategory = React.createClass({
+  getInitialState() {
+    return {
+      name: ''
+    }
+  },
   render() {
     return (
-      <div className="AddCategory" onClick={this.addCategory}>
-        Category
+      <div className="AddCategory">
+        <input className="input-sm" placeholder="name"
+          onChange={this.onChange} />
+        <div className="btn btn-sm btn-success" onClick={this.addCategory}
+          disabled={!this.state.name} >
+          Add
+        </div>
       </div>
     );
   },
+  onChange(e) {
+    // set the state to user input
+    this.setState({
+      name: e.target.value
+    });
+  },
   addCategory() {
-    ViewActionCreators.addCategory();
+    ViewActionCreators.addCategory({
+      name: this.state.name
+    });
   }
 });
 
