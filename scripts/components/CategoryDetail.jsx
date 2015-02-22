dcf9773c823445299de8d5f41868bd95632af6ad
Shirley Wu <shirleyxueyangwu@gmail.com>
2015-02-21 21:40:16 -0800

    categorydetail

---
+++ scripts/components/CategoryDetail.jsx
@@ -0,0 +1,13 @@
+var React = require('react/addons');
+var ViewActionCreators = require('../actions/ViewActionCreators');
+
+var CategoryDetail = React.createClass({
+  render() {
+    return (
+      <div className="CategoryDetail">
+      </div>
+    );
+  }
+});
+
+module.exports = CategoryDetail;
