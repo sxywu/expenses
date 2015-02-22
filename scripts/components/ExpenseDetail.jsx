fb9a7ef21faebb0f6cb1ca0fe983a5ebc5f0d2e1
Shirley Wu <shirleyxueyangwu@gmail.com>
2015-02-22 10:26:10 -0800

    create expense detail component

---
+++ scripts/components/ExpenseDetail.jsx
@@ -0,0 +1,13 @@
+var React = require('react/addons');
+var ViewActionCreators = require('../actions/ViewActionCreators');
+
+var ExpenseDetail = React.createClass({
+  render() {
+    return (
+      <div className="ExpenseDetail">
+      </div>
+    );
+  }
+});
+
+module.exports = ExpenseDetail;
