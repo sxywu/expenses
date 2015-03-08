841eeac29f868463976ca939ae6cf8de45f425e5
Shirley Wu <shirleyxueyangwu@gmail.com>
2015-03-08 15:56:02 -0700

    Finish directions

--- scripts/components/Home.jsx
+++ scripts/components/Home.jsx
@@ -7,9 +7,6 @@ var Home = React.createClass({
   render() {
     return (
       <div className="Home">
-        <h4 className="Home-header">
-          Getting started
-        </h4>
         {this.renderDirections()}
 
         <h4 className="Home-header">
@@ -26,15 +23,15 @@ var Home = React.createClass({
       "glyphicon": true,
       "glyphicon-plus-sign": true
     });
-    var categoryStyle = {width: 50, height: 38};
-    var categoryData = {name: 'Coffee', size: 7.5, x: 25, y: 10, fill: '#3B8686', selected: true};
+    var categoryStyle = {width: 50, height: 40};
+    var categoryData = {name: 'Coffee', size: 7.5, x: 25, y: 11, fill: '#FF8800', selected: true};
     var category = (
       <svg style={categoryStyle}>
         <CategoryComponent data={categoryData} />
       </svg>
     );
-    var expenseStyle = {width: 50, height: 30};
-    var expenseData = {name: 'Coffee Bar', size: 10, x: 25, y: 8, selected: true};
+    var expenseStyle = {width: 50, height: 32};
+    var expenseData = {name: 'Coffee Bar', size: 10, x: 25, y: 9, selected: true};
     var expense = (
       <svg style={expenseStyle}>
         <ExpenseComponent data={expenseData} />
@@ -42,13 +39,24 @@ var Home = React.createClass({
     );
     return (
       <div className="Home-body">
+        <h4>Start</h4>
         <p>
-          <span className={addClasses} />From the Add tab,<br />
+          From the Add tab:<br />
           {category} add some categories,<br />
           {expense} and then add some expenses.<br />
         </p>
+        <h4>Categorize</h4>
         <p>
-
+          Add an expense by dragging it over a category.
+          Drag the expense over a linked category to remove it.
+          An expense can belong to multiple categories.
+        </p>
+        <h4>Inspect</h4>
+        <p>
+          The graph shows a week of expenses at a time.
+          Expenses are placed by time of creation.
+          Categories are placed at the bottom, and only appear when there are expenses linked to it.
+          Click on any expense or category to view them in detail.
         </p>
       </div>
     );
