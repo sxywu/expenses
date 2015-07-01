47a630b38e0161d2226badab382dd638aba78ac0
Shirley Wu <shirleyxueyangwu@gmail.com>
2015-06-30 23:50:22 -0700

    update "about" section

--- scripts/components/Home.jsx
+++ scripts/components/Home.jsx
@@ -37,7 +37,6 @@ var Home = React.createClass({
       <div style={homeStyle}>
         <div className="Home-backdrop" onClick={this.closeModal} />
         <div className="Home-body" ref="body" style={bodyStyle}>
-          <div className="Home-close glyphicon glyphicon-remove" onClick={this.closeModal} />
           {this.renderDirections()}
           {this.renderAbout()}
         </div>
@@ -66,17 +65,20 @@ var Home = React.createClass({
     return (
       <div className="Directions-body">
         <h3>Add</h3>
+        <h5>
+        Add an expense or category from the left panel.
+        </h5>
         <img src="images/add2.gif" />
         <h3>Drag</h3>
         <h5>
-          Add an expense by dragging it over a category, drag again to remove.<br />
+          Drag an expense over a category to add to the category, drag again to remove from the category.
           An expense can belong to multiple categories.
         </h5>
         <img src="images/drag.gif" />
         <h3>Click</h3>
         <h5>
-          Click on any expense or category to view it in detail, click again to deselect.<br />
-          While in detail view, take actions (delete, edit, close).<br />
+          Click on any expense or category to view it in detail, click again to deselect.
+          While in detail view, take actions (delete, edit, close).
           The graph shows a week of expenses at a time.
         </h5>
         <img src="images/click.gif" />
@@ -88,11 +90,15 @@ var Home = React.createClass({
       <div className="About-body">
         <h3>About</h3>
         <h5>
-        This simple expense-tracking app started out as an example app for a blog post on <a href="http://d3js.org/" target="_new">D3</a>+ 
+        This simple expense-tracking app is an accompaniment to a blog post on <a href="http://d3js.org/" target="_new">D3</a>+ 
         <a href="http://facebook.github.io/react/" target="_new">React</a>+ 
         <a href="http://facebook.github.io/flux/docs/overview.html" target="_new">Flux</a>.
-        The blog post itself has yet to be written, but you can find the <a href="https://github.com/sxywu/expenses" target="_new">source code here</a>.
+        The blog post itself is being slowly written, but in the meanwhile, you can find the <a href="https://github.com/sxywu/expenses" target="_new">source code here</a>.
         </h5>
+        <h5>
+        A caveat: there's no backend implemented, so all of the data is stored locally on the client.
+        </h5>
+        <br />
         <h5>
         <a href="https://twitter.com/shirleyxywu" target="_new">Let me know what you think</a><br />
         <a href="https://github.com/sxywu/expenses/issues" target="_new">Tell me if you find a bug</a><br />
